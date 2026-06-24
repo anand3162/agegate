692e3e8c553baaaf463bc1e64fa74b5bce5e2456
anand3162 <anand04krishna@gmail.com>
2026-06-24 10:41:39 -0400

    Day 2: Add GET /api/scans route with hardcoded fake data

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,14 @@ app.get('/api/health', (req,res) => {
     res.status(200).json({ status: 'ok' });
 });
 
+app.get('/api/scans', (req, res) => {
+    res.status(200).json([
+  { "staffId": "test123", "estimatedAge": "22-26", "recommendation": "Check ID" },
+  { "staffId": "test123", "estimatedAge": "34-38", "recommendation": "Looks Clear" }
+]
+);
+});
+
 app.post('/api/scans', (req, res) => {
   console.log('Scan received:', req.body);
   res.status(201).json({ message: 'Scan received' });
